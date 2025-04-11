6e8f68a3d4a291375ac418313d9ae192843c5ace
victor.lightbase <victor.lightbase@sebraemg.com.br>
2025-04-11 00:23:31 -0300

    Implementa whitelist no ValidationPipe global

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    app.useGlobalPipes(new ValidationPipe())
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true, //remove campos passados nao listados em dto
+            forbidNonWhitelisted: true // dispara erro caso campo nao listado em dto seja passado
+        })
+    )
     await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
